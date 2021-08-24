6a228d529215a7689fda5acfa68193c1c84647da
Martin Albert <martin.albert.187@gmail.com>
2021-08-24 14:08:56 +0200

    feat: update geocoding profile and remove old maps

--- superface/sdk.d.ts
+++ superface/sdk.d.ts
@@ -1,8 +1,3 @@
 export { AddressGeocodingProfile } from "./types/address/geocoding";
-export declare const SuperfaceClient: new () => import("@superfaceai/one-sdk/dist/client/public/client").TypedSuperfaceClient<{
-    "address/geocoding": {
-        Geocode: [import("./types/address/geocoding").AddressGeocodingGeocodeInput, import("./types/address/geocoding").AddressGeocodingGeocodeResult];
-        ReverseGeocode: [import("./types/address/geocoding").AddressGeocodingReverseGeocodeInput, import("./types/address/geocoding").AddressGeocodingReverseGeocodeResult];
-    };
-}>;
+export declare const SuperfaceClient: any;
 export declare type SuperfaceClient = InstanceType<typeof SuperfaceClient>;
